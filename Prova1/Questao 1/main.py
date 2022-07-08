2da56b16d1c033a9c15872d11fba741b053df74b
Daniel Fazzioni <daniel.fazzioni@gmail.com>
2022-07-08 16:53:07 -0300

    Creating Tbutton

--- Prova1/Questao 1/main.py
+++ Prova1/Questao 1/main.py
@@ -1,41 +1,81 @@
 import pygame as pg
 
+#cores
+clblack = (0,0,0)
+clwhite = (255,255,255)
 
-class Tobject():
+Objetos = []
+
+class Tcontrol():
     def __init__(self):
         self.visible = True
-        self.enabled = True
-        self.rect = 0,0,0,0
+        self.Enabled = True
+        self.tag = 0
+        self.caption = ""
+        Objetos.append(self)
 
-    def PosInside(self,pos):
-        return (pos[0] >= rect[0]) and (pos[0] <= rect[0]+rect[2])
+# Criar Classes para controlar a UAI
+class Trect():
+    """
+        Interface responsavel por guardar as dimensoes dos objetos
+    """
+    def __init__(self,left,top,width = 0,heigth = 0):
+        self.left = left
+        self.top = top
+        self.heigth = heigth
+        self.width = width
+    def rect(self):
+        return [self.left,self.top, self.width,self.heigth]
+    def size(self):
+        return self.width,self.heigth
+    def Inside(self,pos):
+        return (self.left  > pos[0] > self.left+self.width) and (self.top > pos[1] > self.heigth+self.top)
 
 
-class Tbutton( Tobject ):
-    def __init__(self, rect ):
-        Tobject()    
+class Tlabel():
+    def __init__(self,txt,pos):
+       self.txt = txt
+       self.pos = pos 
+
+
+
+class TButton( Tcontrol, Trect ):
+    
+    def __init__(self):
+        Tcontrol.__init__(self)
+        Trect.__init__(self,0, 0)
+        
+        # print( self.visible)
+
+
 
 
 def start():
+
     pg.init()
     size  = width,heigth = 800,600
-
     screen = pg.display.set_mode(size=size)    
 
+    b = TButton()
+    b.left = 20
+    b.top = 50
+    b.width = 100
+    b.heigth = 20
+ 
+    print(Objetos[0].rect())
+
     while True:
-
+        screen.fill((0,0,0))
         # Buscar os eventos do usuario
-        
-
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+            if event.type == pg.QUIT: #Sai do jogo
                 exit()
             elif event.type == pg.KEYUP:
                 print(event)
-            # <Event(1025-MouseButtonDown {'pos': (324, 200), 'button': 1, 'touch': False, 'window': None})>
-            
 
-        screen.fill((0,0,0))
+            # <Event(1025-MouseButtonDown {'pos': (324, 200), 'button': 1, 'touch': False, 'window': None})>
+
+        pg.draw.rect(screen,clwhite, b.rect() )
         pg.display.flip()
 
 
